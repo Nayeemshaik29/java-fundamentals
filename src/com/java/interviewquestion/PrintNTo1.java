e3e5fa93bd42679e51c6aade5651c4965f58bd15
Nayeemshaik29 <bashanayeem79@gmail.com>
2026-01-21 11:21:27 +0530

    Added 2 Question in Level-1

---
+++ src/com/java/interviewquestion/PrintNTo1.java
@@ -0,0 +1,17 @@
+package com.java.interviewquestion;
+
+import java.util.Scanner;
+
+public class PrintNTo1 {
+    public static void main(String[] args) {
+        {
+            Scanner sc=new Scanner(System.in);
+            int n =sc.nextInt();
+            int sum = 0;
+            for (int i = n; i > 0; i--) {
+                sum+=i;
+            }
+            System.out.println(sum);
+        }
+    }
+}
