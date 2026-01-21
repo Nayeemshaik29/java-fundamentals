62b318fdd21c31adc66a154dd15e8f38eaa5f114
Nayeemshaik29 <bashanayeem79@gmail.com>
2026-01-21 11:33:46 +0530

    Added 3 Question in Level-1

---
+++ src/com/java/interviewquestion/PrintEvenFrom1ToN.java
@@ -0,0 +1,17 @@
+package com.java.interviewquestion;
+
+import java.util.Scanner;
+
+public class PrintEvenFrom1ToN {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            if(i%2==0){
+                System.out.println(i);
+            }
+
+        }
+
+    }
+}
